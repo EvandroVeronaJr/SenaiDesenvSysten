d159c564b0827f09e522ded681d3e83238c210dc
DESKTOP-ME7S2FS\Evandro Jr <verona.junior@gmail.com>
2021-08-05 21:56:52 -0300

    corrigindo classe ADM

--- Java/Models/Admin.java
+++ Java/Models/Admin.java
@@ -2,14 +2,25 @@ package Java.Models;
 
 public class Admin extends User {
 
+	private boolean isAdm;
+
 	public Admin() {
 
 	}
 
 	// Construtor com herança da classe User
 	public Admin(int id, String username, String firstName, String lastName, String email, String password,
-			String phone, int userStatus) {
+			String phone, int userStatus, boolean isAdm) {
 		super(id, username, firstName, lastName, email, password, phone, userStatus);
+		this.isAdm = isAdm;
+	}
+
+	public boolean isAdm() {
+		return isAdm;
+	}
+
+	public void setAdm(boolean isAdm) {
+		this.isAdm = isAdm;
 	}
 
 }// Fim classe
